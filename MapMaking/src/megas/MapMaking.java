008e5a0ba4ac11c9ae17764754d291391cc4dee4
Nazmican Çalık <nazmicancalik@gmail.com>
2018-12-16 19:05:16 +0300

    Added first files

---
+++ MapMaking/src/megas/MapMaking.java
@@ -0,0 +1,146 @@
+package megas;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+import lejos.robotics.chassis.Chassis;
+import lejos.robotics.chassis.Wheel;
+import lejos.robotics.chassis.WheeledChassis;
+import lejos.robotics.navigation.MovePilot;
+import lejos.utility.Delay;
+import lejos.utility.PilotProps;
+
+public class MapMaking {
+	
+	public static final double TRAVEL_CHUNK = 4.0;
+	public static final double TURN_RIGHT_ANGLE = 75.0;
+	public static final double STEP_TURN_ANGLE = 5.0;
+	public static final int ONE_SIDE_TRAVEL_COUNT = (int) (100/TRAVEL_CHUNK);
+	
+	public static final int LINEAR_SPEED = 5;
+	public static final int ANGULAR_SPEED = 25;
+
+	
+	static EV3 ev3 = (EV3) BrickFinder.getDefault();
+	
+	static EV3UltrasonicSensor ultrasonicSensor = new EV3UltrasonicSensor(SensorPort.S1);
+	
+	static MovePilot pilot;
+	
+	static float getUltrasonicSensorValue() {
+		SampleProvider sampleProvider = ultrasonicSensor.getDistanceMode();
+		if(sampleProvider.sampleSize() > 0) {
+			float [] samples = new float[sampleProvider.sampleSize()];
+			sampleProvider.fetchSample(samples, 0);
+			return samples[0];
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) throws Exception {		
+		EV3 ev3 = (EV3) BrickFinder.getDefault();
+		GraphicsLCD graphicsLCD = ev3.getGraphicsLCD();
+		
+		graphicsLCD.clear();
+		graphicsLCD.drawString("Map Making", graphicsLCD.getWidth()/2, 0, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
+		
+		PilotProps pilotProps = new PilotProps();
+		pilotProps.setProperty(PilotProps.KEY_WHEELDIAMETER, "4.96");
+		pilotProps.setProperty(PilotProps.KEY_TRACKWIDTH, "11.94");
+		pilotProps.setProperty(PilotProps.KEY_LEFTMOTOR, "A");
+		pilotProps.setProperty(PilotProps.KEY_RIGHTMOTOR, "D");
+		pilotProps.setProperty(PilotProps.KEY_REVERSE, "false");
+		pilotProps.storePersistentValues();
+		pilotProps.loadPersistentValues();
+    	
+    	EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+    	EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+    	
+    	float leftWheelDiameter = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_WHEELDIAMETER, "5.72"));
+    	float rightWheelDiameter = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_WHEELDIAMETER, "5.28"));
+    	
+    	float trackWidth = Float.parseFloat(pilotProps.getProperty(PilotProps.KEY_TRACKWIDTH, "11.95"));
+    	boolean reverse = Boolean.parseBoolean(pilotProps.getProperty(PilotProps.KEY_REVERSE, "false"));
+    	
+    	Chassis chassis = new WheeledChassis(new Wheel[]{WheeledChassis.modelWheel(leftMotor,leftWheelDiameter).offset(-trackWidth/2).invert(reverse),WheeledChassis.modelWheel(rightMotor,rightWheelDiameter).offset(trackWidth/2).invert(reverse)}, WheeledChassis.TYPE_DIFFERENTIAL);
+    	
+    	pilot = new MovePilot(chassis);
+    	pilot.setLinearSpeed(LINEAR_SPEED);
+    	pilot.setAngularSpeed(ANGULAR_SPEED);
+    	pilot.stop();
+		
+		ServerSocket serverSocket = new ServerSocket(1234);
+		
+		graphicsLCD.clear();
+		graphicsLCD.drawString("Map Making", graphicsLCD.getWidth()/2, 0, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
+		graphicsLCD.drawString("Waiting", graphicsLCD.getWidth()/2, 20, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
+		graphicsLCD.refresh();
+		
+		Socket client = serverSocket.accept();
+		
+		graphicsLCD.clear();
+		graphicsLCD.drawString("Map Making", graphicsLCD.getWidth()/2, 0, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
+		graphicsLCD.drawString("Connected", graphicsLCD.getWidth()/2, 20, GraphicsLCD.VCENTER|GraphicsLCD.HCENTER);
+		graphicsLCD.refresh();
+        
+		OutputStream outputStream = client.getOutputStream();
+		DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+		int currentSide = 0;
+		int position = 0;
+		while (Button.readButtons() != Button.ID_ESCAPE) {
+			
+			for (int i = 0; i < 4; i++) {
+				
+				// Update Current Side
+				currentSide = i;
+				position = 0;
+				
+				for(int j = 0; j < ONE_SIDE_TRAVEL_COUNT; j++) {
+					pilot.travel(TRAVEL_CHUNK);	
+					
+					// Update position
+					position = (int) (TRAVEL_CHUNK * j);
+					
+					dataOutputStream.writeInt(currentSide);
+					dataOutputStream.writeInt(position);
+					dataOutputStream.writeFloat(getUltrasonicSensorValue());
+					dataOutputStream.flush();
+				}
+				turnRight();
+			}
+			
+			
+			Delay.msDelay(500);
+		}
+		
+		dataOutputStream.close();
+		serverSocket.close();
+	}
+	
+	public static void turnRight() {
+		/*gyroSensor.reset();
+		float currentAngle = getAngle();
+		while (Math.abs(currentAngle)< TURN_RIGHT_ANGLE) {
+			pilot.rotate(STEP_TURN_ANGLE);
+			currentAngle = getAngle();
+		}*/
+		pilot.rotate(TURN_RIGHT_ANGLE);
+	}
+	/*
+	public static float getAngle() {
+		SampleProvider sampleProvider = gyroSensor.getAngleMode();
+		float[] sample = new float[sampleProvider.sampleSize()];
+		sampleProvider.fetchSample(sample, 0);
+		return sample[0];
+	}*/
+}
---
+++ MapMakingPc/src/megas/MapMakingPc.java
@@ -0,0 +1,178 @@
+package megas;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Line2D;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MapMakingPc extends JFrame {
+	
+	
+	// ========================= MAP DATA ==============================
+	public static final int FRAME_WIDTH = 800;
+	public static final int FRAME_HEIGHT = 800;
+	
+	public static final int SQUARE_WIDTH = 300;
+	public static final int SQUARE_HEIGHT= SQUARE_WIDTH;
+	public static final int SQUARE_X = (FRAME_WIDTH / 2) - (SQUARE_WIDTH / 2);
+	public static final int SQUARE_Y = SQUARE_X;
+	
+	public static final int ENVIRONMENT_RECT_WIDTH = 10;
+	public static final int ENVIRONMENT_RECT_HEIGHT = 10;
+	
+	public static final int FACTOR = 1;
+	
+	public static final int SCALE_FACTOR = SQUARE_WIDTH / 100;
+	
+	// ========================= MAP DATA ==============================
+	
+	public static ArrayList<Point> environment = new ArrayList<>();
+	
+	private static final long serialVersionUID = 2280874288993963333L;
+
+	static InputStream inputStream;
+	static OutputStream outputStream;
+	static DataInputStream dataInputStream;
+	
+	static int side = 0;
+	static int position = 0;
+	static float distance = 0;
+
+	public MapMakingPc() {
+		super("Map Making");
+		setSize( 700, 500 );
+		
+		JPanel panel = new JPanel();
+
+		this.getContentPane().add(panel);
+		setVisible(true);	
+	}
+	
+	public static void main(String[] args) throws Exception	{
+		
+		MapMakingPc monitor = new MapMakingPc();
+		
+		monitor.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		
+		// Enter the ip here.
+		String ip = "10.0.1.1";
+		
+		@SuppressWarnings("resource")
+		Socket socket = new Socket(ip, 1234);
+		System.out.println("Connected!");
+		
+		inputStream = socket.getInputStream();
+		outputStream = socket.getOutputStream();
+		dataInputStream = new DataInputStream(inputStream);
+		DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+		
+		while( true ){
+			side = dataInputStream.readInt();
+			position = dataInputStream.readInt();
+			distance = dataInputStream.readFloat()*100;
+			
+			System.out.println("Side     : " + side);
+			System.out.println("Position : " + position);
+			System.out.println("Distance : " + distance);
+			System.out.println("=====================================");
+			monitor.repaint();
+			dataOutputStream.flush();
+		}
+	}
+	
+	public void paint(Graphics g) {
+		// super.repaint();
+		super.paint(g);
+		displayTrack(g);
+		displayMegas(g);
+		displayEnvironment(g);
+	}
+	
+	// Finds the position of Megas and Draw the position.
+	public void displayMegas(Graphics g) {
+		Graphics2D g2 = ( Graphics2D ) g;
+		g2.setPaint( Color.BLUE );
+		g2.setStroke( new BasicStroke( 7.0f ));
+		
+		int megas_X = 0;
+		int megas_Y = 0;
+		
+		int shownPosition = position * SCALE_FACTOR;
+		
+		// Calculate the position of Megas.
+		if(side == 0) {
+			megas_Y = SQUARE_Y + SQUARE_HEIGHT;
+			megas_X = SQUARE_X + SQUARE_WIDTH - shownPosition;
+		} else if (side == 1) {
+			megas_Y = SQUARE_Y + SQUARE_HEIGHT - shownPosition;
+			megas_X = SQUARE_X;
+		} else if (side == 2) {
+			megas_Y = SQUARE_Y;
+			megas_X = SQUARE_X + shownPosition;
+		} else {
+			megas_Y = SQUARE_Y + shownPosition;
+			megas_X = SQUARE_X + SQUARE_WIDTH;
+		}
+		
+		
+		// Draw Megas
+		g2.draw(new Rectangle(megas_X, megas_Y,2,2));
+	}
+	
+	public void displayEnvironment(Graphics g) {
+		Graphics2D g2 = ( Graphics2D ) g;
+		g2.setPaint( Color.GREEN);
+		g2.setStroke( new BasicStroke( 5.0f ));
+		
+		int obstacle_X = 0;
+		int obstacle_Y = 0;
+		
+		int shownPosition = position * SCALE_FACTOR;
+		
+		// Calculate the position of Megas.
+		if(side == 0) {
+			obstacle_Y = (int) (SQUARE_Y + SQUARE_HEIGHT + (distance / FACTOR));
+			obstacle_X = SQUARE_X + SQUARE_WIDTH - shownPosition;
+		} else if (side == 1) {
+			obstacle_Y = SQUARE_Y + SQUARE_HEIGHT - shownPosition;
+			obstacle_X = (int) (SQUARE_X - (distance / FACTOR));
+		} else if (side == 2) {
+			obstacle_Y = (int) (SQUARE_Y - (distance / FACTOR));
+			obstacle_X = SQUARE_X + shownPosition;
+		} else {
+			obstacle_Y = SQUARE_Y + shownPosition;
+			obstacle_X = (int) (SQUARE_X + SQUARE_WIDTH + (distance / FACTOR));
+		}
+		
+		environment.add(new Point(obstacle_X,obstacle_Y));
+		
+		for (int i=0;  i<environment.size(); i++) {
+			g2.draw(new Rectangle(environment.get(i).x,environment.get(i).y, 4, 4));	
+		}
+	}
+	
+	// Prints the square path.
+	public void displayTrack(Graphics g) {
+		Graphics2D g2 = ( Graphics2D ) g;
+		g2.setPaint( Color.ORANGE );
+		g2.setStroke( new BasicStroke( 5.0f ));
+		
+		g2.draw(new Rectangle(SQUARE_X,SQUARE_Y, SQUARE_WIDTH, SQUARE_HEIGHT));
+	}
+}
+
